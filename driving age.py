f301ac97bb554ef5637bf3c2bd0f972aaa5c8759
Marc Gonzalez <40756@longroad.ac.uk>
2014-09-30 10:24:30 +0100

    selection exercise

---
+++ driving age.py
@@ -0,0 +1,12 @@
+#Marc Gonzalez
+#30/09/14
+#selection exercise 1.2
+
+age = int(input("Please enter your age: "))
+
+if age > 16 :
+    print("you are old enough to drive")
+
+else:
+    print("You are not old enough to drive")
+    
