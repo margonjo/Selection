4f689d9f2c23a994faf7951e4aca0ddefb29ee95
Marc Gonzalez <40756@longroad.ac.uk>
2014-09-30 10:31:51 +0100

    selection exercise

---
+++ range of numbers.py
@@ -0,0 +1,15 @@
+#Marc Gonzalez
+#30/09/14
+#selection exercises 1.3
+
+number = float(input("please enter a number between 21 and 29: "))
+
+if number < 21 :
+    print(" The number that you have entered is lower than 21")
+
+elif number > 29 :
+    print(" the number you have entered is larger than 29")
+
+else:
+    print("The number you have entered is between the range of 21 and 29")
+    
