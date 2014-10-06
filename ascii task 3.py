06288a0f8b7754ad927f32c69d479395b9aaf58e
Marc Gonzalez <40756@student.longroad.ac.uk>
2014-10-06 19:02:40 +0100

    ascii

---
+++ ascii task 3.py
@@ -0,0 +1,30 @@
+#Marc Gonzalez
+#6/10/14
+#ascii exercise 3.1
+
+binary = input( "please enter your 8-bit binary number here: ")
+                
+ones = 0
+
+
+
+if binary[0] == 1:
+      ones +=1
+elif binary[1] == 1:
+      ones +=1
+elif binary[2] == 1:
+      ones +=1
+elif binary[3] == 1:
+      ones +=1
+elif binary[4] == 1:
+      ones +=1
+elif binary[5] == 1:
+      ones +=1
+elif binary[6]== 1:
+      ones +=1 
+elif binary[7] == 1:
+      ones +=1
+
+
+
+print (ones)
