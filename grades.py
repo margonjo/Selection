9a0344cee316ba71d4a17f6da6e494c101f34f3e
Marc Gonzalez <40756@student.longroad.ac.uk>
2014-10-06 19:13:41 +0100

    exercise 2.4

---
+++ grades.py
@@ -0,0 +1,24 @@
+#Marc Gonzalez
+#06/10/14
+#exercise 2.4
+
+grade = int(input("please enter your grade: "))
+
+if  81<= grade<=100:
+    print("you have an A")
+
+elif 71<= grade <= 80:
+    print("you have a B")
+
+elif 61<= grade<= 70:
+    print ("you have a C")
+
+elif 51<= grade<= 60:
+    print("you have a D")
+
+elif 41<= grade <= 50:
+    print("you have a E")
+else:
+    print("you have a U")
+    
+
