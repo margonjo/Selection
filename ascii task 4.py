18c45a680f59bbf111b3fe3acb148a08a52ec1c6
Marc Gonzalez <40756@longroad.ac.uk>
2014-10-07 09:06:09 +0100

    ascii

---
+++ ascii task 4.py
@@ -0,0 +1,36 @@
+#Marc Gonzalez
+#6/10/14
+#ascii exercise 4.1
+
+binary = input( "please enter your 7-bit binary number here: ")
+                
+ones = 0
+
+
+
+if binary[0] == 1:
+      ones +=1
+elif binary[1] == 1:
+      ones +=1
+elif binary[2] == 1:
+      ones +=1
+elif binary[3] == 1:
+      ones +=1
+elif binary[4] == 1:
+      ones +=1
+elif binary[5] == 1:
+      ones +=1
+elif binary[6]== 1:
+      ones +=1 
+else:
+    + 0
+if int (1)== even:
+    +"1" binary
+
+else:
+    +"0" binary
+
+
+
+
+print (ones)
