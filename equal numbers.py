a3d23e362809dbad5cf556c0e54b13acfd2bacc0
Marc Gonzalez <40756@student.longroad.ac.uk>
2014-10-08 21:23:38 +0100

    selection

---
+++ equal numbers.py
@@ -0,0 +1,13 @@
+#Marc Gonzalez
+#7/10/14
+#selection 1.1
+
+number1 = float(input("please enter a number: "))
+number2 = float(input("please enter another number: "))
+
+if number1 == number2:
+    print("The numbers you have entered are equal")
+
+else:
+    print("the numbers you have entered are not equal")
+    
