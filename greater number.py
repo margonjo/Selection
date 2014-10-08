8e83ab386716c4d2a2d191dfe5874ea581e3d44d
Marc Gonzalez <40756@student.longroad.ac.uk>
2014-10-08 21:23:21 +0100

    selection

---
+++ greater number.py
@@ -0,0 +1,12 @@
+#Marc Gonzalez
+#8/10/14
+#selection 1.4
+
+number1 = float(input("please enter a number: "))
+number2 = float(input("please enter another number: "))
+
+if number1 > number2:
+    print(number1)
+
+else:
+    print(number2)
